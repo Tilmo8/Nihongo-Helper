dac9582f33c7c0e464e4bdc659bb50be0f316298
Unknown <santino_gaara@yahoo.com>
2019-05-09 01:00:28 +0800

    View tests, etc.

--- Project Files/kanjireading/views.py
+++ Project Files/kanjireading/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse	# Remove later if HttpResponse is not needed
 
 # Create your views here.
+def index(request):
+	return HttpResponse("Test success. Rendered \"kanji-reading.index\".")
