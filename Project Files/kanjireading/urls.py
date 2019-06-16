df875c5d2d38e29a49a7c2dfea3dc28279f7ae75
Tino <santino_gaara@yahoo.com>
2019-06-16 22:22:02 +0800

    Create simple views & templates

--- Project Files/kanjireading/urls.py
+++ Project Files/kanjireading/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('random', views.random_kanji, name='random'),
+    path('detail/<str:kanji>', views.detail, name='detail')
 ]
--- Project Files/kanjireading/views.py
+++ Project Files/kanjireading/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse	# Remove later if HttpResponse is not needed
+from django.http import HttpResponse, Http404	# Remove later if HttpResponse is not needed
+from django.template import loader
+
+from .models import Kanji
+
+import random as Random
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Test success. Rendered \"kanji-reading.index\".")
+
+def detail(request, kanji):
+	if not isinstance(kanji, Kanji):
+		kanji_lookup = Kanji.objects.filter(character__exact=kanji)
+
+		if not kanji_lookup.exists():
+			raise Http404("Kanji not found")
+		else:
+			kanji = kanji_lookup[0]
+
+	template = loader.get_template('kanjireading/detail.html')
+	context = {
+		'kanji': kanji
+	}
+
+	return HttpResponse(template.render(context, request))
+	# return HttpResponse(f"<h1>HAI, you're looking at kanji: {kanji.character} <h1>")
+
+def random_kanji(request):
+	kanji_list = list(Kanji.objects.all())
+
+	list_length = len(kanji_list)
+
+	if list_length <= 0:
+		raise Http404("Kanji not found")
+
+	kanji = Random.choice(kanji_list)
+
+	template = loader.get_template('kanjireading/detail.html')
+	context = {
+		'kanji': kanji
+	}
+
+	return HttpResponse(template.render(context, request))
+
+
